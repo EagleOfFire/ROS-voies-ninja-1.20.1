62256ca5a00e3275ccdd73abe8c5831483f5cd1c
EagleOfFire <maxoucrafteur83@gmail.com>
2024-07-20 11:43:39 +0200

    small change

--- src/main/java/ros/eagleoffire/rosvoiesninjas/event/SceauScellementInteractionEventHandler.java
+++ src/main/java/ros/eagleoffire/rosvoiesninjas/event/SceauScellementInteractionEventHandler.java
@@ -29,19 +29,15 @@ public class SceauScellementInteractionEventHandler {
         BlockPos pos = event.getPos();
         AABB boundingBox = new AABB(pos.offset(-1, -1, -1), pos.offset(2, 2, 2)); // Cover a larger area around the block
 
-        System.out.println("Block right-clicked at position: " + pos);
-
         List<Entity> entities = event.getLevel().getEntities((Entity) null, boundingBox);
 
         boolean hasHangingEntity = entities.stream().anyMatch(entity ->
                 entityClasses.stream().anyMatch(clazz -> clazz.isInstance(entity)));
 
         if (hasHangingEntity) {
-            System.out.println("Hanging entity found, cancelling event.");
             event.setCanceled(true);
-            event.getEntity().displayClientMessage(Component.nullToEmpty("You can't interact with this block as it has a hanging entity!"), true);
+            event.getEntity().displayClientMessage(Component.nullToEmpty("RATIO !"), true);
         } else {
-            System.out.println("No hanging entity found. Entities in bounding box: ");
             for (Entity entity : entities) {
                 System.out.println(" - " + entity.getType().getDescription() + " at position " + entity.blockPosition());
             }        }
